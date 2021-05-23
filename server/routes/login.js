c4115010f7280fe564e0ff3523c62393b4f783ca
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-24 02:00:28 +0300

    login route done
    Co-Authored-By: Connor <cimok2000@gmail.com>

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,41 +7,35 @@ const router = express.Router();
 
 // Functions
 async function createToken(user, res) {
-    user = user[0]
-  
     const payload = {
-      uuid: user.uuid,
+      uuid: user._id,
       username: user.username,
     };
   
-    jwt.sign(payload, "h4x0r", { expiresIn: "30d" }, (err, token) => {
+    jwt.sign(payload, process.env.SECRET, { expiresIn: "30d" }, (err, token) => {
       res.status(200)
          .cookie("token", token)
          .json({
             message: "Logged in",
             token: token,
-            uuid: user.uuid,
-            user: user
           });
     });
 }
 
 router.post("/login", async (req, res) => {
     // Parse body
-    fuck123
-    const body = req.body;
-    const user = await db.users.find(
-      { username: body.username },
-      { collation: { locale: "en", strength: 2 } }
-    );
-    
+    const user = (await User.find({ username: req.body.username }).exec())[0];
+
+    console.log(req.body.username, user);
+
     try {  // Try matching
-      const match = await bcrypt.compare(body.password, user[0].settings.account.password);
+      const match = await bcrypt.compare(req.body.password, user.password);
       if (match) createToken(user, res);
       else throw "error"; // If password doesn't match throw error
     } catch (error) {
       console.log(error);
-      if (error) res.status(200).json({ error: "Invalid Credentials ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡" });
+      if (error) res.status(400).json({ error: "Invalid Credentials ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡" });
     }
 });
+
 module.exports = router;
