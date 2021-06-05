07ee46d05c0c2e4311a1355646f79bcfd5876d66
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-05 08:03:31 +0300

    better response message

--- server/routes/login.js
+++ server/routes/login.js
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
   const user = await User.findOne({ username: req.body.username }).exec();
 
   // If there is no user with those credentials return this
-  if (!user) return res.status(400).json({ error: "Invalid Credentials ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡" });
+  if (!user) return res.status(400).json({ error: "User not found ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡" });
 
   // This is for backwards compatibility
   if (!user.geolocation) await User.update(user._id, { geolocation: req.body.geolocation });
