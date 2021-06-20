06959be72c68dffbb96f8b15c82ef9b146b242e8
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-20 22:19:44 +0300

    redo this later properly

--- server/index.js
+++ server/index.js
@@ -47,15 +47,15 @@ app.use(
 );
 
 
-app.get("/stats", async (req, res) => {
-  let object = {
-    totalMachines: machines.size,
-    totalTraffic: ((await Stats.fetchDailyTraffic(86400000)).total_megabytes / 1000).toFixed(2),
-    totalCores: Array.from(machinesStatic.values()).reduce((a, b) => a + b.static.cpu.cores, 0),
-    totalRam: Math.ceil(Array.from(machines.values()).reduce((a, b) => a + b.ram.total, 0)),
-  };
-  res.json(object);
-});
+// app.get("/stats", async (req, res) => {
+//   let object = {
+//     totalMachines: machines.size,
+//     totalTraffic: ((await Stats.fetchDailyTraffic(86400000)).total_megabytes / 1000).toFixed(2),
+//     totalCores: Array.from(machinesStatic.values()).reduce((a, b) => a + b.static.cpu.cores, 0),
+//     totalRam: Math.ceil(Array.from(machines.values()).reduce((a, b) => a + b.ram.total, 0)),
+//   };
+//   res.json(object);
+// });
 
 app.use(require("@/routes/login"));
 app.use(require("@/routes/signup"));
