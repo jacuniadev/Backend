e0e0d65b6d5cb2ec3bee60b0f40da509d7225028
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-04 19:07:58 +0200

    update readme

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
+	// Clear terminal
+	fmt.Print("\033[H\033[2J")
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-			return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello, World 👋!")
 	})
 
 	app.Listen(":3000")
