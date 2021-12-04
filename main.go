7de5632e80cd8fa7ae2219a613cea024e0199f5f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-04 20:12:53 +0200

    implement mongo

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = client.Disconnect(ctx) }()
 
 	// Clear terminal
 	fmt.Print("\033[H\033[2J")
