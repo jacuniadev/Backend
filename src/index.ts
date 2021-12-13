b669953b8c8507d5a9e68bcac9cfec043b766242
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-13 13:36:05 +0200

    remove uncommented code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Server } from "./classes/server";
 
 async function main() {
+  console.clear();
   const server = new Server(8000);
 }
 
--- src/routes/v1/users.route.ts
+++ src/routes/v1/users.route.ts
@@ -4,43 +4,28 @@ import { UserSignupInput, UserObject, UserLoginInput } from "../../types/user";
 
 export const users: Router = express.Router();
 
-users.get<{}, UserObject[]>(
-  "/@all",
-  async (req, res) => getUsers().then((users) => res.json(users))
-  // .catch(() => res.status(500).send())
+users.get<{}, UserObject[]>("/@all", async (req, res) => getUsers().then((users) => res.json(users)));
+
+users.delete<{}, { message: string }>("/@all", async (req, res) =>
+  deleteAllUsers().then(() => res.json({ message: "success" }))
 );
 
-users.delete<{}, { message: string }>(
-  "/@all",
-  async (req, res) => deleteAllUsers().then(() => res.json({ message: "success" }))
-  // .catch(() => res.status(500).send())
+users.post<{}, { user: UserObject } | { message: string }, UserSignupInput>("/@signup", async (req, res) =>
+  createUser(req.body).then(
+    (user) => res.status(201).json({ user }),
+    (reason) => res.status(400).json({ message: reason })
+  )
 );
 
-users.post<{}, { user: UserObject } | { message: string }, UserSignupInput>(
-  "/@signup",
-  async (req, res) =>
-    createUser(req.body).then(
-      (user) => res.status(201).json({ user }),
-      (reason) => res.status(400).json({ message: reason })
-    )
-  // .catch(() => res.status(500).send())
+users.post<{}, { user: UserObject; token: string } | { message: string }, UserLoginInput>("/@login", async (req, res) =>
+  loginUser(req.body).then(
+    (data) => res.status(200).json(data),
+    (reason) => res.status(400).json({ message: reason })
+  )
 );
 
-users.post<{}, { user: UserObject; token: string } | { message: string }, UserLoginInput>(
-  "/@login",
-  async (req, res) =>
-    loginUser(req.body).then(
-      (data) => res.status(200).json(data),
-      (reason) => res.status(400).json({ message: reason })
-    )
-  // .catch(() => res.status(500).send())
-);
-
-users.get(
-  "/@search/:by/:query",
-  async (req, res) =>
-    getUser({ [req.params.by]: req.params.query }).then((user) =>
-      user !== null ? res.json(user) : res.status(404).json({ message: "user not found" })
-    )
-  // .catch(() => res.status(500).send())
+users.get("/@search/:by/:query", async (req, res) =>
+  getUser({ [req.params.by]: req.params.query }).then((user) =>
+    user !== null ? res.json(user) : res.status(404).json({ message: "user not found" })
+  )
 );
