319a3d366aa78109f2671912386872b0eb59185f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-13 04:12:44 +0200

    refactored some of the tests

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -18,6 +18,14 @@ function checkUserNotFound(response: request.Response) {
   expect(body.message).to.be.deep.equal("User not found");
 }
 
+async function signup() {
+  const { body, status }: { body: UserObject; status: number } = await request(server).post("/users/@signup").send(userPayload);
+  return {
+    status,
+    body,
+  };
+}
+
 describe("🚀 Test Server Endpoints", () => {
   describe("GET /", () => {
     it("message should be Hello World", async () => {
@@ -34,68 +42,71 @@ describe("🚀 Test Server Endpoints", () => {
   describe("/users", () => {
     describe("POST /@signup", () => {
       it("should have status of 201", async () => {
-        const response = await request(server).post("/users/@signup").send(userPayload);
-        expect(response.status).to.be.equal(201);
+        const { status } = await signup();
+        expect(status).to.be.equal(201);
       });
       it("created_at should exist", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.created_at).to.exist;
       });
       it("updated_at should exist", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.updated_at).to.exist;
       });
       it("username should be equal to the payload", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.username).to.be.deep.equal(userPayload.username);
       });
       it("email should be equal to the payload", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.email).to.be.deep.equal(userPayload.email);
       });
       it("avatar should be undefined", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.avatar).to.be.undefined;
       });
       it("biography should be undefined", async () => {
-        const { body }: { body: UserObject } = await request(server).post("/users/@signup").send(userPayload);
+        const { body } = await signup();
         expect(body.biography).to.be.undefined;
       });
     });
 
     describe("GET /@all", () => {
+      beforeEach(async () => await createUser(userPayload));
+
       it("should have status of 200", async () => {
-        await createUser(userPayload);
         const response = await request(server).get("/users/@all");
         expect(response.status).to.be.equal(200);
       });
 
       it("should be an array of users", async () => {
-        await createUser(userPayload);
         const response = await request(server).get("/users/@all");
         expect(response.body).to.be.not.empty;
       });
     });
 
     describe("DELETE /@all", () => {
+      beforeEach(async () => await createUser(userPayload));
+
       it("should return a json message saying success", async () => {
-        await createUser(userPayload);
         const response = await request(server).delete("/users/@all");
-        expect(response.status).to.be.equal(200);
         expect(response.body).to.be.deep.equal({ message: "success" });
       });
 
+      it("should have a status of 200", async () => {
+        const response = await request(server).delete("/users/@all");
+        expect(response.status).to.be.equal(200);
+      });
+
       it("shouldnt be any users left", async () => {
-        await createUser(userPayload);
         await request(server).delete("/users/@all");
-        const response1 = await request(server).get("/users/@all");
-        expect(response1.status).to.be.equal(200);
-        expect(response1.body).to.be.deep.equal([]);
+        const response = await request(server).get("/users/@all");
+        expect(response.body).to.be.deep.equal([]);
       });
     });
 
-    describe("GET /@search/:by/:query =>", () => {
-      describe("given valid email input", () => {
+    describe("GET /@search/:by/:query", () => {
+      describe("with valid email input", () => {
         it("should return a user if it exists", async () => {
           await createUser(userPayload);
           const response = await request(server).get(`/users/@search/email/${userPayload.email}`);
