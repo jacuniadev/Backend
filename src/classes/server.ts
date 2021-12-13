9ee0dc7c21ef7dc00e230b1242e7c38572c8b078
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-13 09:48:23 +0200

    install cors

--- src/classes/server.ts
+++ src/classes/server.ts
@@ -1,9 +1,10 @@
-import express, { Express, Router, Response, Request } from "express";
+import express, { Express } from "express";
 import http from "http";
+import cors from "cors";
 import { v1 } from "../routes/v1";
 
 export class Server {
-  public express: Express = express().use(express.json()).use(v1);
+  public express: Express = express().use(express.json()).use(v1).use(cors);
   public server = http
     .createServer(this.express)
     .listen(this.PORT, () => console.log(`[INDEX] Started on port ${this.PORT.toString()}`));
