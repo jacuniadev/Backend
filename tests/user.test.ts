18c43ddbcc107cee2f61e86e33809abe222a38a6
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-13 05:13:39 +0200

    organized tests a lil better

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -25,69 +25,71 @@ export const userPayload = {
 };
 
 describe("📔 User Database Functions & Methods", () => {
-  describe("createUser()", () => {
-    describe("given valid input", () => {
-      it("should have a hash password of length 60", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.password).to.have.lengthOf(60);
-      });
-      it("username should match the payload username", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.username).to.be.equals(userPayload.username);
-      });
-      it("email should match the payload email", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.email).to.be.equals(userPayload.email);
-      });
-      it("should have a filled created_at field", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.created_at).to.be.exist;
-      });
-      it("should have a filled updated_at field", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.updated_at).to.be.exist;
-      });
-      it("should not init with an avatar", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.avatar).to.be.undefined;
-      });
-      it("should not init with a biography", async () => {
-        const user: UserDocument = await createUser(userPayload);
-        expect(user.biography).to.be.undefined;
+  describe("📔 Statics", () => {
+    describe("createUser()", () => {
+      describe("given valid input", () => {
+        it("should have a hash password of length 60", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.password).to.have.lengthOf(60);
+        });
+        it("username should match the payload username", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.username).to.be.equals(userPayload.username);
+        });
+        it("email should match the payload email", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.email).to.be.equals(userPayload.email);
+        });
+        it("should have a filled created_at field", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.created_at).to.be.exist;
+        });
+        it("should have a filled updated_at field", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.updated_at).to.be.exist;
+        });
+        it("should not init with an avatar", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.avatar).to.be.undefined;
+        });
+        it("should not init with a biography", async () => {
+          const user: UserDocument = await createUser(userPayload);
+          expect(user.biography).to.be.undefined;
+        });
       });
     });
-  });
 
-  describe("getUser()", () => {
-    describe("given valid input", () => {
+    describe("getUser()", () => {
+      describe("given valid input", () => {
+        beforeEach(async () => await createUser(userPayload));
+        it("should find a user by username", async () => expect(await getUser({ username: userPayload.username })).to.exist);
+        it("should find a user by email", async () => expect(await getUser({ username: userPayload.username })).to.exist);
+      });
+    });
+
+    describe("loginUser()", () => {
       beforeEach(async () => await createUser(userPayload));
-      it("should find a user by username", async () => expect(await getUser({ username: userPayload.username })).to.exist);
-      it("should find a user by email", async () => expect(await getUser({ username: userPayload.username })).to.exist);
+      describe("given a valid password", () =>
+        it("should return true", async () =>
+          expect(await loginUser({ email: userPayload.email, password: userPayload.password })).to.be.true));
+
+      describe("given an invalid password", () =>
+        it("should return false", async () =>
+          expect(await loginUser({ email: userPayload.email, password: "wrong" })).to.be.false));
+    });
+
+    describe("deleteAllUsers()", () => {
+      it("should leave the database empty", async () => {
+        await createUser(userPayload);
+        await deleteAllUsers();
+        const users = await getUsers();
+        expect(users).to.be.empty;
+      });
     });
   });
 
-  describe("loginUser()", () => {
-    beforeEach(async () => await createUser(userPayload));
-    describe("given a valid password", () =>
-      it("should return true", async () =>
-        expect(await loginUser({ email: userPayload.email, password: userPayload.password })).to.be.true));
-
-    describe("given an invalid password", () =>
-      it("should return false", async () =>
-        expect(await loginUser({ email: userPayload.email, password: "wrong" })).to.be.false));
-  });
-
-  describe("deleteAllUsers()", () => {
-    it("should leave the database empty", async () => {
-      await createUser(userPayload);
-      await deleteAllUsers();
-      const users = await getUsers();
-      expect(users).to.be.empty;
-    });
-  });
-
-  describe("update user", () => {
-    describe("updatePassword()", () => {
+  describe("📔 Methods", () => {
+    describe("user.updatePassword()", () => {
       it("should get rehashed & different", async () => {
         const user: UserDocument = await createUser(userPayload);
         const oldPasswordHash = user.password;
@@ -97,7 +99,7 @@ describe("📔 User Database Functions & Methods", () => {
       });
     });
 
-    describe("updateEmail()", () => {
+    describe("user.updateEmail()", () => {
       it("should be different", async () => {
         const user: UserDocument = await createUser(userPayload);
         const oldEmail = user.email;
@@ -106,7 +108,7 @@ describe("📔 User Database Functions & Methods", () => {
       });
     });
 
-    describe("updateUsername()", () => {
+    describe("user.updateUsername()", () => {
       it("should be different", async () => {
         const user: UserDocument = await createUser(userPayload);
         const oldUsername = user.username;
@@ -115,7 +117,7 @@ describe("📔 User Database Functions & Methods", () => {
       });
     });
 
-    describe("updateBiography()", () => {
+    describe("user.updateBiography()", () => {
       it("should be different", async () => {
         const user: UserDocument = await createUser(userPayload);
         const oldBiography = user.biography;
