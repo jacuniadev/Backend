060092ba609ba181067b16e71358dff0e20cd8e1
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-14 07:04:30 +0200

    use actual mongo url in here

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Backend } from "./classes/backend";
+import { MONGO_URL } from "./constants";
 
 async function main() {
   console.clear();
-  const backend = Backend.create({ port: 8000, verbose: true });
+  const backend = Backend.create({ port: 8000, verbose: true, mongoUrl: MONGO_URL });
 }
 
 main();
