79acd3f2bd02f3abf8e770645f9c67a954935a37
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-14 07:12:09 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+// TODO: Move these to a config of some sort
 export const JWT_SECRET = "asscheeks26";
 export const MONGO_URL = "mongodb://localhost/xornet";
 export const MONGO_TESTING_URL = "mongodb://localhost/xornet-testing";
