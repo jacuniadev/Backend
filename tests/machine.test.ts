8850cb62b2bb04c3ab62ccc22b937cf366fe1d6b
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-17 18:07:35 +0200

    some more unit tests

--- tests/machine.test.ts
+++ tests/machine.test.ts
@@ -53,6 +53,33 @@ describe("Machine Database Functions & Methods", () => {
         it("'icon' shouldn't exist yet", () => expect(response.icon).to.not.exist);
         it("'access' should be an empty array", () => expect(response.access).to.be.empty);
       });
+
+      describe("given an invalid hostname", () => {
+        it("should return an error saying 'hostname is invalid'", () =>
+          createMachine({
+            hardware_uuid: "5852a4fe-6b5e-4d40-8c4b-78bccc7d65c6",
+            owner_uuid: user.uuid,
+            hostname: "",
+          }).catch((error) => expect(error).to.be.equal("hostname is invalid")));
+      });
+
+      describe("given an invalid owner_uuid", () => {
+        it("should return an error saying 'owner_uuid' is invalid'", () =>
+          createMachine({
+            hardware_uuid: "5852a4fe-6b5e-4d40-8c4b-78bccc7d65c6",
+            owner_uuid: "xxx",
+            hostname: "Xena",
+          }).catch((error) => expect(error).to.be.equal("owner_uuid is invalid")));
+      });
+
+      describe("given an invalid hardware_uuid", () => {
+        it("should return an error saying 'hardware_uuid' is invalid'", () =>
+          createMachine({
+            hardware_uuid: "xxx",
+            owner_uuid: "5852a4fe-6b5e-4d40-8c4b-78bccc7d65c6",
+            hostname: "Xena",
+          }).catch((error) => expect(error).to.be.equal("hardware_uuid is invalid")));
+      });
     });
   });
 });
