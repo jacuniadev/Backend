33b316dafaacc36b68b642bd33d66a871aafada4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-18 04:28:42 +0200

    implement websocket types & auth validation
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- src/classes/websocketManager.class.ts
+++ src/classes/websocketManager.class.ts
@@ -1,28 +1,58 @@
 import ws, { RawData } from "ws";
+import mitt, { Emitter } from "mitt";
+import { DynamicData, StaticData } from "../types/machine";
+import { loginMachine } from "../services/machine.service";
+
+export interface WebsocketEvent<ID extends EventID, T> {
+  e: ID;
+  data: T;
+}
+
+export enum EventID {
+  login = 0x01,
+  statics = 0x04,
+  dynamicData = 0x05,
+}
+
+export interface Events {
+  login: WebsocketEvent<EventID.login, { access_token: string }>;
+  statics: WebsocketEvent<EventID.statics, StaticData>;
+  dynamicData: WebsocketEvent<EventID.statics, DynamicData>;
+}
 
 export class WebsocketConnection {
+  public mitt = mitt<{
+    [k in keyof Events]: Events[k]["data"];
+  }>();
+  public is_authenticated = false;
+
   constructor(public socket: ws) {
     console.log("Reporter Connected");
-    let hostname = "Unknown";
     socket.on("message", (message) => {
-      const data = this.parseData(message);
-      // if (data?.statics?.hostname) hostname = data.statics.hostname;
-      console.log(
-        `${hostname}: ${message.toString().length} bytes CPU: ${
-          data.cpu?.usage.reduce((a: any, b: any) => a + b, 0) / data.cpu?.usage.length
-        }`
-      );
-      console.log(data);
+      const { e, data } = this.parseData(message);
+      const id = EventID[e];
+
+      if (!this.is_authenticated && id !== "login") return socket.close();
+
+      this.mitt.emit(id as keyof typeof EventID, data);
     });
   }
 
   private parseData(data: RawData) {
-    return JSON.parse(data.toString());
+    return JSON.parse(data.toString()) as WebsocketEvent<EventID, any>;
   }
 }
 
 export class WebsocketManager {
   constructor(public server: ws.Server) {
-    this.server.on("connection", (socket) => new WebsocketConnection(socket));
+    this.server.on("connection", (socket) => {
+      const ws = new WebsocketConnection(socket);
+      ws.mitt.on("login", async (data) => {
+        if (await loginMachine(data.access_token)) {
+          ws.is_authenticated = true;
+          console.log("the bitch is authed");
+        }
+      });
+    });
   }
 }
--- src/services/machine.service.ts
+++ src/services/machine.service.ts
@@ -1,7 +1,7 @@
 import { KeyManager } from "../classes/keyManager.class";
 import Machine from "../models/machine.model";
 import { UserObject } from "../types/user";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "../constants";
 import { FilterQuery } from "mongoose";
 import { CreateMachineInput, MachineDocument } from "../types/machine";
@@ -16,7 +16,7 @@ export const createMachine = async (input: CreateMachineInput) => {
   if (!isUUIDValid(input.owner_uuid)) return Promise.reject("owner_uuid is invalid");
   if (!isHostnameValid(input.hostname)) return Promise.reject("hostname is invalid");
 
-  const access_token = jwt.sign(input.hardware_uuid, JWT_SECRET);
+  const access_token = jwt.sign(input, JWT_SECRET);
   return Machine.create({
     access_token,
     hardware_uuid: input.hardware_uuid,
@@ -34,3 +34,17 @@ export const create2FAKey = (user: UserObject): { key: string } => {
 export const check2FAKey = (key: string) => keyManager.validate(key);
 
 export const deleteAllMachines = () => Machine.deleteMany({});
+
+/**
+ * Attempts to login a machine
+ */
+export const loginMachine = async (access_token: string): Promise<boolean> => {
+  try {
+    const { hardware_uuid, owner_uuid, hostname } = jwt.verify(access_token, JWT_SECRET) as CreateMachineInput;
+    const machine = await Machine.findOne({ hardware_uuid, owner_uuid, hostname, access_token });
+    if (!machine) return Promise.reject("invalid credentials");
+    return true;
+  } catch (error) {
+    return Promise.reject("invalid credentials");
+  }
+};
