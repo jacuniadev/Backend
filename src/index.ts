9f766d3dd89266633852888bae9a9a4152fd36d4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-28 02:17:26 +0200

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ async function main() {
   const backend = await Backend.create({
     port: 8085,
     verbose: true,
-    mongoUrl: process.env.MONGO_URL || "mongodb://127.0.0.1/xornet",
+    mongoUrl: process.env.MONGO_URL!,
   });
   // express().use(express.json()).use(v1).listen(8080);
 }
