dc174ab690acccfe6d72fdcf6cb08e0dd83182e2
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-01-22 19:09:32 +0200

    removed some useless fields to cut down on network traffic
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>
    Co-Authored-By: Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>

--- src/classes/websocketManager.class.ts
+++ src/classes/websocketManager.class.ts
@@ -74,19 +74,16 @@ export class WebsocketManager {
           socket.socket.close();
         }
       });
-      socket.on("staticData", (data) => {
-        updateStaticData(machineUUID!, data);
-      });
+
+      socket.on("staticData", (data) => updateStaticData(machineUUID!, data));
+
       socket.on("dynamicData", (data) => {
         const computedData = {
           ...data,
-          uuid: machineUUID,
           cpu_average_usage: data.cpu.usage.reduce((a, b) => a + b, 0) / data.cpu.usage.length,
           cpu_average_speed: data.cpu.freq.reduce((a, b) => a + b, 0) / data.cpu.usage.length,
           total_download: data.network.reduce((a, b) => a + b.rx, 0) / 1000 / 1000,
           total_upload: data.network.reduce((a, b) => a + b.tx, 0) / 1000 / 1000,
-          ram_used_gb: data.ram.used / 1024 / 1024,
-          ram_total_gb: data.ram.total / 1024 / 1024,
         };
 
         this.machineDynamicData[machineUUID!] = computedData;
