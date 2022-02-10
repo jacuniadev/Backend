d5bbd5741ffbf668995a9b50a8287a0a451531bd
SVRGE <geoxor123@outlook.com>
2022-02-10 15:22:15 -0500

    remove extra semi colin

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -42,35 +42,35 @@ const userSchema = new mongoose.Schema({
   client_settings: {
     general: {
       opacity: { type: Number },
-      theme: { type: String},
-      enable_bloom: { type: Boolean},
-      enable_rounded_corners: { type: Boolean},
-      enable_sound_effects: { type: Boolean},
-      enable_status_bar: { type: Boolean},
-      show_offline_machines: { type: Boolean},
-      show_owned_machines_only: { type: Boolean},
+      theme: { type: String },
+      enable_bloom: { type: Boolean },
+      enable_rounded_corners: { type: Boolean },
+      enable_sound_effects: { type: Boolean },
+      enable_status_bar: { type: Boolean },
+      show_offline_machines: { type: Boolean },
+      show_owned_machines_only: { type: Boolean },
     },
     columns: {
-      hostname: { type: Boolean},
-      cpu_average_usage: { type: Boolean},
-      cpu_average_speed: { type: Boolean},
-      ram_usage: { type: Boolean},
-      gpu_usage: { type: Boolean},
-      gpu_power_usage: { type: Boolean},
-      network_switch: { type: Boolean},
-      download: { type: Boolean},
-      upload: { type: Boolean},
-      temperature: { type: Boolean},
-      country: { type: Boolean},
-      public_ip: { type: Boolean},
-      process_count: { type: Boolean},
-      host_uptime: { type: Boolean},
-      reporter_uptime: { type: Boolean},
-      reporter_version: { type: Boolean},
-      owner: { type: Boolean},
-      action: { type: Boolean},
-    };
-  }
+      hostname: { type: Boolean },
+      cpu_average_usage: { type: Boolean },
+      cpu_average_speed: { type: Boolean },
+      ram_usage: { type: Boolean },
+      gpu_usage: { type: Boolean },
+      gpu_power_usage: { type: Boolean },
+      network_switch: { type: Boolean },
+      download: { type: Boolean },
+      upload: { type: Boolean },
+      temperature: { type: Boolean },
+      country: { type: Boolean },
+      public_ip: { type: Boolean },
+      process_count: { type: Boolean },
+      host_uptime: { type: Boolean },
+      reporter_uptime: { type: Boolean },
+      reporter_version: { type: Boolean },
+      owner: { type: Boolean },
+      action: { type: Boolean },
+    },
+  },
 });
 
 userSchema.pre("save", async function (this: UserDocument, next) {
@@ -120,10 +120,13 @@ userSchema.methods.updateBiography = async function (this: UserDocument, newValu
   return this.save();
 };
 
-userSchema.methods.updateClientSettings = async function (this: UserDocument, newValue: UserClientSettings): Promise<UserDocument> {
+userSchema.methods.updateClientSettings = async function (
+  this: UserDocument,
+  newValue: UserClientSettings
+): Promise<UserDocument> {
   this.client_settings = newValue;
   return this.save();
-}
+};
 
 userSchema.methods.getMachines = async function (this: UserDocument): Promise<MachineDocument[]> {
   // return getMachines({ $or: [{ owner_uuid: this.uuid }, { access: this.uuid }] });
