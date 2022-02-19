944c2ed2f2a7cbd913335c14abae7f4222e0892f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-19 17:34:21 -0500

    add some more metrics for debugging

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,6 +4,13 @@ import { newUserBackend } from "./users.route";
 
 export const v1: Router = express.Router();
 
-v1.get("/", async (req, res) => res.json({ message: "Hello World" }));
+v1.get("/", async (req, res) =>
+  res.json({
+    message: "Hello World",
+    memory: process.memoryUsage(),
+    uptime: process.uptime(),
+    cpu: process.cpuUsage(),
+  })
+);
 v1.use("/users", newUserBackend());
 v1.use("/machines", machines);
