041126b30c1ff94d3391ea6e4b01ad19a7c48a3f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-21 08:19:16 -0500

    update scripts for local development

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,24 @@ import { Backend } from "./classes/backend.class";
 async function main() {
   console.clear();
 
-  process.on("uncaughtException", (err) => {
-    console.error("Uncaught Exception: ", err.message);
-  });
+  process.on("uncaughtException", (err) => console.error("Uncaught Exception: ", err.message));
 
-  const backend = await Backend.create({
-    port: 8085,
-    secure: true,
-    verbose: true,
-    mongoUrl: process.env.MONGO_URL!,
-  });
+  const backend =
+    process.env.MODE === "production"
+      ? Backend.create({
+          port: 8085,
+          secure: true,
+          verbose: true,
+          mongoUrl: process.env.MONGO_URL!,
+        })
+      : Backend.create({
+          port: 7000,
+          secure: false,
+          verbose: true,
+          mongoUrl: process.env.MONGO_TESTING_URL! || "mongodb://127.0.0.1:27017",
+        });
+
+  await backend;
 }
 
 main();
