e635efa9a8d5291da31c170b39ba2c8c794e980c
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-24 11:58:14 -0500

    Revert "refactor route registration"
    This reverts commit 609bd4fbdde32eff6969e61bc3f7ee69b584e726.

--- routes/v1/v1.go
+++ routes/v1/v1.go
@@ -1,8 +1,6 @@
 package v1
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/xornet-cloud/Backend/database"
 )
@@ -11,44 +9,23 @@ type V1 struct {
 	db database.Database
 }
 
-type Register struct {
-	verb     string
-	endpoint string
-	handler  func(c *fiber.Ctx) error
-}
-
 func New(db database.Database, app *fiber.App) V1 {
 	var v1 = V1{
 		db,
 	}
 
-	var registers = [9]Register{
-		{"get", "/v1/ping", v1.Ping},
-		{"get", "/v1/ping", v1.Ping},
-		{"get", "/v1/status", v1.Status},
-		{"get", "/v1/users/all", v1.GetUsersAll},
-		{"get", "/v1/users/uuid/:uuid", v1.GetUserByUuid},
-		{"get", "/v1/users/email/:email", v1.GetUserByEmail},
-		{"get", "/v1/users/username/:username", v1.GetUserByUsername},
-		{"post", "/v1/auth/user/login", v1.LoginUser},
-		{"post", "/v1/auth/user/signup", v1.SignupUser},
-	}
+	app.Get("/v1/ping", v1.Ping)
+	app.Get("/v1/status", v1.Status)
 
-	for _, v := range registers {
-		switch v.verb {
-		case "get":
-			app.Get(v.endpoint, v.handler)
-		case "post":
-			app.Post(v.endpoint, v.handler)
-		case "put":
-			app.Put(v.endpoint, v.handler)
-		case "patch":
-			app.Patch(v.endpoint, v.handler)
-		case "delete":
-			app.Delete(v.endpoint, v.handler)
-		}
-		fmt.Printf(" Registered %v %v\n", v.verb, v.endpoint)
-	}
+	app.Get("/v1/users/all", v1.GetUsersAll)
+	app.Get("/v1/users/uuid/:uuid", v1.GetUserByUuid)
+	app.Get("/v1/users/email/:email", v1.GetUserByEmail)
+	app.Get("/v1/users/username/:username", v1.GetUserByUsername)
+
+	app.Post("/v1/auth/user/login", v1.LoginUser)
+	app.Post("/v1/auth/user/signup", v1.SignupUser)
+
+	// app.Patch("/me", v1.UpdateAvatar)
 
 	return v1
 }
