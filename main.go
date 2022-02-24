dc0c9869bdba3a47fdb529208f0768fa7a623e8d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-24 05:08:38 -0500

    finished ricing the backend

--- main.go
+++ main.go
@@ -9,16 +9,34 @@ import (
 	v1 "github.com/xornet-cloud/Backend/routes/v1"
 )
 
+const APP_NAME = "Xornet Backend"
+const CLEAR_SCREEN = "\033[H\033[2J"
+const CYAN_COLOR = "\033[36m"
 const MONGO_URL = "mongodb://localhost:27017"
 const PORT = 3000
+const LOGO = `
+   _  __                      __ 
+  | |/ /___  _________  ___  / /_
+  |   / __ \/ ___/ __ \/ _ \/ __/
+ /   / /_/ / /  / / / /  __/ /_    ___           __               __  
+/_/|_\____/_/  /_/ /_/\___/\__/   / _ )___ _____/ /_____ ___  ___/ / 
+                                 / _  / _ ` + ` / __/  '_/ -_) _ \/ _  /  
+                Made in Golang  /____/\_,_/\__/_/\_\\__/_//_/\_,_/   
+`
 
 func main() {
+
+	print(CLEAR_SCREEN)
+	println(string(CYAN_COLOR), LOGO)
+
 	db, err := database.Connect(MONGO_URL)
 	if err != nil {
 		println("Database failed to connect")
 		panic(1)
 	}
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		AppName: APP_NAME,
+	})
 
 	app.Use(middleware.ErrorHandlerMiddleware)
 	v1.New(*db, app)
