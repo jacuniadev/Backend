5cb0e4a71c80fe84aef2abaafabd9b398c2115ee
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-02 02:06:37 +0300

    update auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,20 +1,21 @@
 import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { getUser } from "../services/user.service";
-import { LoggedInRequest, UserObject } from "../types/user";
+import { DatabaseManager, IUser } from "../database/DatabaseManager";
+import { LoggedInRequest } from "../types/user";
+export const auth = (db: DatabaseManager) => {
+  return async (req: LoggedInRequest, res: Response, next: NextFunction) => {
+    if (req.headers.authorization) {
+      try {
+        const payloadUser = jwt.verify(req.headers.authorization.replace("Bearer ", ""), process.env.JWT_SECRET!) as IUser;
+        const user = await db.find_user_by_uuid(payloadUser.uuid);
 
-export default async (req: LoggedInRequest, res: Response, next: NextFunction) => {
-  if (req.headers.authorization) {
-    try {
-      const payloadUser = jwt.verify(req.headers.authorization.replace("Bearer ", ""), process.env.JWT_SECRET!) as UserObject;
-      const user = await getUser({ _id: payloadUser._id });
-
-      if (!user) return res.status(403).json({ message: "user not found" });
-      req.user = user;
-      return next();
-    } catch (error) {
-      return res.status(403).json({ message: "invalid authentication token" });
+        if (!user) return res.status(403).json({ message: "user not found" });
+        req.user = user;
+        return next();
+      } catch (error) {
+        return res.status(403).json({ message: "invalid authentication token" });
+      }
     }
-  }
-  return res.status(403).json({ message: "authorization header not set" });
+    return res.status(403).json({ message: "authorization header not set" });
+  };
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,5 @@
 import { Request } from "express";
-import mongoose from "mongoose";
-import { DatabaseObject } from ".";
-import { MachineDocument } from "./machine";
+import { ISafeUser, IUser } from "../database/DatabaseManager";
 
 export interface UserLoginInput {
   [key: string]: any;
@@ -18,27 +16,15 @@ export interface UserSignupInput extends UserLoginInput {
 }
 
 /**
- * This is the safe object that will be sent through the API endpoints
- */
-export interface UserObject extends DatabaseObject {
-  [key: string]: any;
-  avatar?: string;
-  biography?: string;
-  email: string;
-  client_settings: string;
-  username: string;
-}
-
-/**
  * The object the login/signup database statics return
  */
-export type UserLoginResult = { user: UserDocument; token: string };
+export type UserLoginResult = { user: IUser; token: string };
 export type UserSignupResult = UserLoginResult;
 
 /**
  * The object the login/signup routes return
  */
-export type UserLoginResultSafe = { user: UserObject; token: string };
+export type UserLoginResultSafe = { user: ISafeUser; token: string };
 export type UserSignupResultSafe = UserLoginResultSafe;
 
 /**
@@ -46,24 +32,5 @@ export type UserSignupResultSafe = UserLoginResultSafe;
  */
 export interface LoggedInRequest extends Request {
   params: any;
-  user?: UserDocument;
-}
-
-/**
- * The backend user containing methods
- */
-// prettier-ignore
-export interface UserDocument extends UserSignupInput, UserObject, mongoose.Document {
-  comparePassword:      (candidatePassword: string)    => Promise<boolean>;
-  updateAvatar:         (newValue: string)             => Promise<UserDocument>;
-  updateBanner:         (newValue: string)             => Promise<UserDocument>;
-  updatePassword:       (newValue: string)             => Promise<UserDocument>;
-  updateEmail:          (newValue: string)             => Promise<UserDocument>;
-  updateUsername:       (newValue: string)             => Promise<UserDocument>;
-  updateBiography:      (newValue: string)             => Promise<UserDocument>;
-  updateClientSettings: (newValue: string)             => Promise<UserDocument>;
-
-  // Machine related stuff
-  createAccessToken:    (hardwareUUID: string)         => Promise<string>;
-  getMachines:          ()                             => Promise<MachineDocument[]>;
+  user?: IUser;
 }
