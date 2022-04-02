2946c509a34fcdaecbc08bf737bd9a2cae834853
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-03 02:04:44 +0300

    simplicity is key

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ async function main() {
   const hostname = `${chalk.cyan("hostname")}  ${chalk.reset(os.hostname())}`;
   const arch = `${chalk.cyan("arch")}      ${chalk.reset(os.arch())}`;
   const pid = `${chalk.cyan("pid")}       ${chalk.reset(process.pid)}`;
-  const secure = `${chalk.cyan("secure")}    ${process.env.SECURE === "true" ? chalk.green("yes") : chalk.red("no")}`;
+  const secure = `${chalk.cyan("secure")}    ${chalk.reset(process.env.SECURE!)}`;
   const mode = `${chalk.cyan("mode")}      ${
     process.env.MODE! === "production" ? chalk.green("production") : chalk.blue("development")
   }`;
