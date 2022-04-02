088edf9e44a93e36708823081a644bf199a13c69
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-02 15:08:30 +0300

    show pid so @N1kO23 is happy

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,16 @@ require("dotenv").config();
 import { Backend } from "./classes/backend.class";
 import chalk from "chalk";
 import { getMemoryUsage } from "./logic";
-
-// get the hostname of the machine
-const hostname = require("os").hostname();
+import os from "os";
 
 async function main() {
   console.clear();
 
   const logo = chalk.blue(`
    _  ______  ___  _  ____________     
-  | |/_/ __ \\/ _ \\/ |/ / __/_  __/    ${chalk.cyan("hostname")}  ${chalk.reset(hostname)}
+  | |/_/ __ \\/ _ \\/ |/ / __/_  __/    ${chalk.cyan("hostname")}  ${chalk.reset(os.hostname())}
  _>  </ /_/ / , _/    / _/  / /       ${chalk.cyan("memory")}    ${chalk.reset((await getMemoryUsage()).used.toFixed(2))} Mb
-/_/|_|\\____/_/|_/_/|_/___/ /_/       
+/_/|_|\\____/_/|_/_/|_/___/ /_/        ${chalk.cyan("pid")}       ${chalk.reset(process.pid)}
   `);
 
   console.log(logo);
