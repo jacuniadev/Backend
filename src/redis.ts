073a21d3f2d6269c19de6b337a063e5c7a14e76f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-19 09:57:10 +0300

    added scripts for running shards

--- src/redis.ts
+++ src/redis.ts
@@ -1,13 +1,10 @@
 import { createClient } from "redis";
 import { Logger } from "./utils/logger";
 
-export const redisSubscriber = createClient({
-  url: "redis://xornet-redis:6379",
-});
+const url = `redis://${process.env.REDIS_HOST || "xornet-redis"}:${process.env.REDIS_PORT || 6379}`;
 
-export const redisPublisher = createClient({
-  url: "redis://xornet-redis:6379",
-});
+export const redisSubscriber = createClient({ url });
+export const redisPublisher = createClient({ url });
 
 redisSubscriber.connect().then(() => Logger.info("Redis Subscriber connected "));
 redisPublisher.connect().then(() => Logger.info("Redis Publisher connected "));
